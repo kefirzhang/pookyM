b9f9265faf6ee0d03babbbf0bab891c384d4f80d
kefirzhang <8390671@qq.com>
2019-08-08 15:15:41 +0800

    do book module

--- app_assets/js/common.js
+++ app_assets/js/common.js
@@ -1,43 +1,43 @@
-(function (doc, win) {
-    $('#list td,#list li').click(function () {
-        location.href = $(this).data('href');
-    });
+$(function(){
+    (function (doc, win) {
+        $('#list td,#list li').click(function () {
+            location.href = $(this).data('href');
+        });
 
-    $('.ui-header .ui-btn').click(function () {
-        location.href = 'index.html';
-    });
+        $('.ui-header .ui-btn').click(function () {
+            location.href = 'index.html';
+        });
 
-    $("#btn1").click(function () {
-        $('.ui-actionsheet').addClass('show');
-    });
+        $("#btn1").click(function () {
+            $('.ui-actionsheet').addClass('show');
+        });
 
-    $("#cancel").click(function () {
-        $(".ui-actionsheet").removeClass("show");
-    });
-    if ($("body").data("module") != "login") {
-        $.ajax({
-            type: 'GET',
-            url: Global.apiUrl + "IsLogin",
-            dataType: 'json',
-            timeout: 300,
-            xhrFields: {
-                withCredentials: true
-            },
-            success: function (response) {
-                if (response.data != "1") {
-                    location.href = '/module/login/login.html';
-                } else {
-                    Global.pageFunc();//执行每个页面的具体函数
+        $("#cancel").click(function () {
+            $(".ui-actionsheet").removeClass("show");
+        });
+        if ($("body").data("module") != "login") {
+            $.ajax({
+                type: 'GET',
+                url: Global.apiUrl + "IsLogin",
+                dataType: 'json',
+                timeout: 300,
+                xhrFields: {
+                    withCredentials: true
+                },
+                success: function (response) {
+                    if (response.data != "1") {
+                        location.href = '/module/login/login.html';
+                    } else {
+                        Global.pageFunc();//执行每个页面的具体函数
+                    }
+                },
+                error: function (xhr, type) {
+                    alert('Ajax error!')
                 }
-                console.log(response)
-            },
-            error: function (xhr, type) {
-                alert('Ajax error!')
-            }
-        })
-    }
-})(document, window);
-
+            })
+        }
+    })(document, window);
+});
 function LoginOut() {
     $.ajax({
         type: 'GET',
